86f70207162f3aa6987f74d4f20ff5a81032f9ba
xrealm <mpging.12315@gmail.com>
2020-07-12 23:03:56 +0800

    feat(filter): add filter

--- media/filter/src/main/java/com/artemis/media/filter/filter/NothingFilter.java
+++ media/filter/src/main/java/com/artemis/media/filter/filter/NothingFilter.java
@@ -3,7 +3,7 @@ package com.artemis.media.filter.filter;
 import com.artemis.media.filter.api.GLTextureInputRenderer;
 
 /**
- * Created by mocmao on 2020/7/110.
+ * Created by xrealm on 2020/7/110.
  */
 public class NothingFilter extends BasicFilter {
 
