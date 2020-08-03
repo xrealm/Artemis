a60ec5738ef7a8aa69bc3cced5b5ff20ada4b4ed
xrealm <mpging.12315@gmail.com>
2020-08-03 20:11:35 +0800

    feat(ar): depth image

--- media/glcore/src/main/java/com/artemis/media/egl/util/GLUtil.java
+++ media/glcore/src/main/java/com/artemis/media/egl/util/GLUtil.java
@@ -2,6 +2,8 @@ package com.artemis.media.egl.util;
 
 import android.opengl.GLES11Ext;
 import android.opengl.GLES30;
+import android.opengl.GLES30;
+import android.util.Log;
 
 /**
  * GL工具类
@@ -14,7 +16,6 @@ public class GLUtil {
     public static int generateOESTexture() {
         int[] textures = new int[1];
         GLES30.glGenTextures(1, textures, 0);
-        GLES30.glActiveTexture(GLES30.GL_TEXTURE0);
         GLES30.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textures[0]);
 
         GLES30.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES30.GL_TEXTURE_MIN_FILTER, GLES30.GL_LINEAR);
@@ -35,5 +36,18 @@ public class GLUtil {
 
     public static void deleteTextures(int n, int[] textures, int offset) {
 
+    }
+
+    public static void checkGLError(String tag, String label) {
+        int lastError = GLES30.GL_NO_ERROR;
+        // Drain the queue of all errors.
+        int error;
+        while ((error = GLES30.glGetError()) != GLES30.GL_NO_ERROR) {
+            Log.e(tag, label + ": glError " + error);
+            lastError = error;
+        }
+        if (lastError != GLES30.GL_NO_ERROR) {
+            throw new RuntimeException(label + ": glError " + lastError);
+        }
     }
 }
